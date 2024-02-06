15613bf1294e53c26c68a5f355dec6efccd196ab
jeongain0909 <128366843+jeongain0909@users.noreply.github.com>
2024-02-06 15:18:03 +0900

    Add files via upload

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const { sequelize } = require("./models");
+
+const userRouter = require("./routes/user");
+
+const app = express();
+
+app.set("port", 3000);
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+app.use("/user", userRouter);
+
+sequelize
+  .sync({ force: false })
+  .then(() => {
+    console.log("DB 연결 성공");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+app.listen(app.get("port"), () => {
+  console.log(`Server On Port ${app.get("port")}`);
+});
